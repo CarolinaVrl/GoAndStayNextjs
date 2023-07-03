2ba9df734be9bd7e668c4e55bfd48bc49e62e75d
Diana Villarreal <dianacarolinav97@gmail.com>
2023-07-03 16:12:49 -0500

    feat: add var global

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors:{
+        'background-principal':'#FFFFFF',
+        'color-primary':'#202F59',
+        'color-secondary':'#5333ED',
+        'dark':'#000000',
+        'gray-1':'#DDE2E5',
+        'gray-2':'#ACB5BD',
+        'gray-4':'#495057',
+      }
       
     },
   },
